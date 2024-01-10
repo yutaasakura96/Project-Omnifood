9b2b24e30660c2883272b52b87c8bb439ff23c5d
yutaasakura96 <yuta.asakura.se@gmail.com>
2024-01-10 21:42:51 +0900

    Implementing smooth scrolling
    -Integrating JavaScript code to achieve smooth scrolling functionality on the webpage.
    -Implementing JavaScript functionality to close the mobile navigation/hamburger menu upon clicking a button within the menu.

--- script.js
+++ script.js
@@ -14,6 +14,34 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+///////////////////////////////////////////////////////////SMOOTH SCROLLING ANIMATION
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // SCROLL TO OTHER LINKS
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // CLOSE MOBILE NAVIGATION
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
